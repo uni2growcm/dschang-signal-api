706bf5090d9689d6a8e984f74429de6b25730a11
LADEESSE <ladeesse.assondji@gaddigital.io>
2026-03-09 10:31:15 +0100

    Scaffold Spring Boot App

---
+++ src/main/java/codylab/dschang_signal/DschangSignalApplication.java
@@ -0,0 +1,13 @@
+package codylab.dschang_signal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DschangSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DschangSignalApplication.class, args);
+	}
+
+}
