d852a553f924bccf05906af75ab327891867590e
rivald123 <stephannintedem@gmail.com>
2026-03-12 14:39:37 +0100

    Backend - Implement API to retrieve public reports

--- src/main/java/u2g/codylab/dschang_signal/service/ReportService.java
+++ src/main/java/u2g/codylab/dschang_signal/service/ReportService.java
@@ -2,13 +2,20 @@ package u2g.codylab.dschang_signal.service;
 
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import u2g.codylab.dschang_signal.dto.ReportApiDTO;
+import u2g.codylab.dschang_signal.dto.UserApiDTO;
+import u2g.codylab.dschang_signal.entity.ModerationStatus;
 import u2g.codylab.dschang_signal.entity.Report;
+import u2g.codylab.dschang_signal.exception.BadRequestException;
 import u2g.codylab.dschang_signal.mapper.ReportMapper;
 import u2g.codylab.dschang_signal.repository.ReportRepository;
+
+import java.util.List;
 
 @Slf4j
 @Transactional
@@ -32,4 +39,19 @@ public class ReportService {
         log.debug("Report with id {} found", id);
         return reportMapper.toReportDTO(report);
     }
+
+    public Page<ReportApiDTO> getPublicReports(Pageable pageable){
+
+        try {
+            Page<ReportApiDTO> dtos = reportRepository.findByModerationStatus(ModerationStatus.RESOLVED,pageable)
+                    .map(reportMapper::toReportDTO);
+            return dtos;
+        } catch (Exception e) {
+            throw new BadRequestException("Invalid pagination parameters");
+        }
+
+
+    }
+
+
 }
