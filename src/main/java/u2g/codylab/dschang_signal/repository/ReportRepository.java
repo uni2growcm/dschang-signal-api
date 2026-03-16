fa833140892c07cd8a7c477552166c8cec04352a
Israel-gloire <i31060995@gmail.com>
2026-03-16 15:55:20 +0100

    reviewer

--- src/main/java/u2g/codylab/dschang_signal/repository/ReportRepository.java
+++ src/main/java/u2g/codylab/dschang_signal/repository/ReportRepository.java
@@ -1,5 +1,4 @@
 package u2g.codylab.dschang_signal.repository;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,12 +6,7 @@ import org.springframework.stereotype.Repository;
 import u2g.codylab.dschang_signal.entity.ModerationStatus;
 import u2g.codylab.dschang_signal.entity.Report;
 
-import java.util.List;
-
-
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
     Page<Report> findByModerationStatus (ModerationStatus moderationStatus, Pageable pageable);
-
-    List<Report> id(Long id);
 }
